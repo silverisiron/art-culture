08998dbf12a2db32a1b612feb3a18250a1dc0f1f
silverisiron <silverisiron@gmail.com>
2024-12-13 18:05:09 +0900

    add modal

---
+++ app.js
@@ -0,0 +1,35 @@
+{ /* 슬라이드 쇼 */
+  let slideIndex = 0;
+  const slides = document.querySelectorAll('.slide');
+  const slideWrapper = document.querySelector('.slide-wrapper');
+  const totalSlides = slides.length;
+  document.getElementById('next').addEventListener('click', showNextSlide);
+  document.getElementById('prev').addEventListener('click', showPrevSlide);
+  function showNextSlide() {
+    console.log(slideIndex);
+    if(slideIndex==0){
+      slideWrapper.style.marginLeft="-100%";
+      slideIndex++;
+    }else if(slideIndex==1){
+      slideWrapper.style.marginLeft="-200%";
+      slideIndex++;
+    }else{
+      slideWrapper.style.marginLeft="0px";
+      slideIndex = 0;
+    }
+  }
+  function showPrevSlide() {
+    console.log(slideIndex);
+    if(slideIndex==1){
+      slideWrapper.style.marginLeft="0px";
+      slideIndex--;
+    }else if(slideIndex==2){
+      slideWrapper.style.marginLeft="-100%";
+      slideIndex--;
+    }else{
+      slideWrapper.style.marginLeft="-200%";
+      slideIndex = 2;
+    }
+  }
+  setInterval(showNextSlide, 10000);
+}
